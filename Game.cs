cce4336fb533f8840297a5a6193131345af84001
Pay-ton <001224922@mohawkcollege.ca>
2026-02-24 18:37:38 -0500

    fix all code, add new code, everything works now.

--- Game.cs
+++ Game.cs
@@ -16,7 +16,7 @@ namespace MohawkGame2D
         // For Circle Detection
         bool ActiveCircle = false;
 
-       
+
 
         // For Detecting mouse location    NEVER FUCKING MIND ACTUALLY, EAT SHIT AND DIE PERHAPS? THATS ALL THIS FUCKING CODE IS TELLING ME IT REFUSES TO WORK
         // float mouseLocationVertical = ;
@@ -49,61 +49,69 @@ namespace MohawkGame2D
             // THIS IS WHAT FIXED THE DUPLICATION ERROR
             Window.ClearBackground(Color.OffWhite);
 
-            
+            // MOUSE POSITION CONVERSION I HOPE
+            float mouseX = Input.GetMouseX();
+            float mouseY = Input.GetMouseY();
+
 
             // Probably also need to spawn the circles under the updates section I imagine
             if (ActiveCircle == false)
             {
                 // For Randomizing Circle Position
                 // Randomized Float Code from module 3, Tweaked slightly
-                float VerticalCirclePosition = Random.Float(1, 200);
-                float HorizontalCirclePosition = Random.Float(1, 200);
+                VerticalCirclePosition = Random.Float(1, 200);
+                HorizontalCirclePosition = Random.Float(1, 200);
                 Window.ClearBackground(Color.OffWhite);
-                Draw.FillColor = Color.Red;
-                Draw.LineColor = Color.Black;
-                Draw.Circle(HorizontalCirclePosition, (VerticalCirclePosition), (25));
-                // ActiveCircle = true;
+
+                ActiveCircle = true;
                 // OF Note, THE ABOVE CODE IS THE CODE THAT STOPS THE SIEZURES. REMOVE THE COMMENT MARKINGS if you'd like, I figured I would leave it in for demonstrative purpose
 
+
             }
+
+            Draw.FillColor = Color.Red;
+            Draw.LineColor = Color.Black;
+            Draw.Circle(HorizontalCirclePosition, (VerticalCirclePosition), (25));
 
             // Draw a circle at mouse position ( EXCEPT SMALLER NOW! ) 
             Draw.FillColor = Color.Green;
             Draw.LineColor = Color.Black;
-            Draw.Circle(Input.GetMouseX(), Input.GetMouseY(), 5);
+            Draw.Circle(mouseX, mouseY, 5);
 
 
+            // For calculating distance between mouse and random circle
+            float DistanceX = HorizontalCirclePosition - mouseX;
+            float DistanceY = VerticalCirclePosition - mouseY;
+
+            // FOR HANDLING NEGATIVE VALUES ON GRID IN DISTANCE MEASUREMENT
+            if (DistanceX < 0)
+            {
+                DistanceX = DistanceX * -1;
+            }
+            if (DistanceY < 0)
+            {
+                DistanceY = DistanceY * -1;
+            }
+
+            float DistanceCheck = DistanceX + DistanceY;
+
+            // DETECTION MAYBE PLEASE???????
+            if (DistanceCheck < 25)
+            {
+                ActiveCircle = false;
+                Console.WriteLine("CIRCLE DETECTED!!!!!!!");
+            }
+            // DETECTION WORKS
+
+
+
+            // UNUSED WASTES OF CODE HELL
+            
             // OK SO I PROBABLY NEED TO MAKE THE MOUSE POSITION BIGGER THAN THE EXACT POSITION. Gonna try and see if I can't do that with below
             // float MouseLocation = (Input.GetMousePosition() + 5) & (Input.GetMousePosition() - 5);  GUESS WHAT? MORE UNUSABLE CODE THAT I'VE ELECTED TO GIVE UP ON
 
-            // MOUSE POSITION CONVERSION I HOPE
-            float mouseX = Input.GetMouseX();
-            float mouseY = Input.GetMouseY();
 
-            float mouseDetectionX = (mouseX + 5);
-            float mouseDetectionY = (mouseY + 5);
-
-            // This needs to be some kind of check for if the Input.Getmouse X and Y == the circle position
-            // GENUINELY WHAT THE FUCK. WHY ISN'T THIS WORKING!?!?!?!?!?
-            if (mouseDetectionX == HorizontalCirclePosition && mouseDetectionY == VerticalCirclePosition)
-            {
-                MouseOverCircle = true;
-            }
-
-            // Realized I can reuse this to remove the circle as part of the detection/Resetting
-            // This SHOULD both clear the screen, and trigger the active circle spawning system. 
-            if (MouseOverCircle == true)
-            {
-                Window.ClearBackground(Color.OffWhite);
-                ActiveCircle = false;
-                MouseOverCircle = false;
-            }
-
-
-
-            // Comment
-
-            bool Unused;
+           
         }
 
     }
